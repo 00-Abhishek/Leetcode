7a7d4272f41be8efaee1a19e80650b09bd0f25d9
Abhishek Pal <dev.04.palabhishek@gmail.com>
2026-07-20 12:06:57 +0530

    Time: 3 ms (83.13%), Space: 19.8 MB (19.55%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.py
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -5,23 +5,17 @@ class Solution:
         rows = len(grid)
         cols = len(grid[0])
 
-        total = rows * cols
+        arr = []
+        for row in grid:
+            arr.extend(row)
+
+        total = len(arr)
         k %= total
 
-        ans = [[0] * cols for _ in range(rows)]
+        arr = arr[-k:] + arr[:-k]
 
-        for i in range(rows):
-            for j in range(cols):
-                # Convert 2D index to 1D
-                idx = i * cols + j
-
-                # Find new index after shifting
-                new_idx = (idx + k) % total
-
-                # Convert new 1D index back to 2D
-                r = new_idx // cols
-                c = new_idx % cols
-
-                ans[r][c] = grid[i][j]
+        ans = []
+        for i in range(0, total, cols):
+            ans.append(arr[i:i + cols])
 
         return ans
